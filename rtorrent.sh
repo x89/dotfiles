8e500a397f7abfef153ee126eaa29c0b6e90a3d1
David John <github@vaunt.eu>
2017-08-24 21:28:36 +0100

    Gotta have this ehH

---
+++ rtorrent.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+TORRENT_PATH="$1"
+TORRENT_NAME="$2"
+TORRENT_LABEL="$3"
+JRE_PATH="/usr/lib/jvm/java-8-openjdk/jre/bin"  # Doesn't work with Java 7 Trol
+
+echo "Torrent Label: ${TORRENT_LABEL}"
+echo "Torrent Name: ${TORRENT_NAME}"
+echo "Torrent Path: ${TORRENT_PATH}"
+
+if [ ! -d "${TORRENT_PATH}" ]; then
+	echo "${TORRENT_PATH} not a valid path"
+	exit
+fi
+
+PATH=${JRE_PATH}:${PATH}
+
+# Grab English and Spanish subtitles
+filebot -get-missing-subtitles "${TORRENT_PATH}" --lang en --output srt --encoding utf8 -non-strict
+filebot -get-missing-subtitles "${TORRENT_PATH}" --lang es --output srt --encoding utf8 -non-strict
+filebot -script "dev:suball" "${TORRENT_PATH}" -non-strict --lang en --encoding utf8 --output srt
+filebot -script "dev:suball" "${TORRENT_PATH}" -non-strict --lang es --encoding utf8 --output srt
+
+filebot -script fn:amc --output "/nas/Multimedia/" --action move -non-strict ${TORRENT_PATH} --def excludeList=/data/amc.txt seriesFormat="/nas/Multimedia/TV/{n}/Season.{s}/{n} - {sxe} - {vf} - {t}" --def movieFormat="/nas/Multimedia/Movies/{ny}/{fn}" --conflict auto --def plex=127.0.0.1:G2xsXDRbpxwsvNLz2Gxe --def clean=y
+
