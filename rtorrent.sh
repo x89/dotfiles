bbbed2ce8e11ab33d60d85d572b37d36ec0b3ebe
David John <github@vaunt.eu>
2017-08-27 21:38:15 +0100

    Update DNS on rtorrent

--- rtorrent.sh
+++ rtorrent.sh
@@ -21,5 +21,5 @@ fi
 filebot -script "dev:suball" -non-strict --lang en --encoding utf8 --output srt "${TORRENT_PATH}"
 filebot -script "dev:suball" -non-strict --lang es --encoding utf8 --output srt "${TORRENT_PATH}"
 
-filebot -script fn:amc --output "/nas/Multimedia/" --action move -non-strict --def excludeList=/data/amc.txt seriesFormat="/nas/Multimedia/TV/{n}/Season.{s}/{n} - {sxe} - {vf} - {t}" --def movieFormat="/nas/Multimedia/Movies/{ny}/{fn}" --conflict auto --def plex=127.0.0.1:G2xsXDRbpxwsvNLz2Gxe --def clean=y ${TORRENT_PATH}
+filebot -script fn:amc --output "/nas/Multimedia/" --action move -non-strict --def excludeList=/data/amc.txt seriesFormat="/nas/Multimedia/TV/{n}/Season.{s}/{n} - {sxe} - {vf} - {t}" --def movieFormat="/nas/Multimedia/Movies/{ny}/{fn}" --conflict auto --def plex=edinburgh.vaunt.eu:G2xsXDRbpxwsvNLz2Gxe --def clean=y ${TORRENT_PATH}
 
